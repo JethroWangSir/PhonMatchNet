7fd6baa65ab74b7c4fd83810da4cec67b75560b2
jethrowang <jethrowang0531@gmail.com>
2025-05-15 16:32:49 +0800

    20250515

--- run_ga.sh
+++ run_ga.sh
@@ -6,9 +6,10 @@ python train_ga.py \
         --epoch 30 \
         --lr 1e-3 \
         --batch_size 512 \
+        --gradient_accumulation_steps 8 \
         --loss_weight 1.0 1.0 \
         --audio_input both \
         --text_input g2p_embed \
         --stack_extractor \
-        --output_dir ./results/maxlen_t_30 \
+        --output_dir ./results/ga \
         --comment 'user comments for each experiment'
